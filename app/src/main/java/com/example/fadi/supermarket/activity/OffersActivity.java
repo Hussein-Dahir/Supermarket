42dc89c48fc1a5a426c6bce0ab6a3c54f4dfba57
Hussein-Dahir <Hussein_Dahir@hotmail.com>
2018-05-16 22:17:35 +0300

    un-needed codes removed + Offers Parser implemented

---
+++ app/src/main/java/com/example/fadi/supermarket/activity/OffersActivity.java
@@ -0,0 +1,63 @@
+package com.example.fadi.supermarket.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.example.fadi.supermarket.R;
+import com.example.fadi.supermarket.adapter.ProductAdapter;
+import com.example.fadi.supermarket.async.task.AsyncResponse;
+import com.example.fadi.supermarket.async.task.GetDataAsyncTaskRunner;
+import com.example.fadi.supermarket.model.Product;
+import com.example.fadi.supermarket.other.Constants;
+import com.example.fadi.supermarket.util.JsonParser;
+
+import java.util.ArrayList;
+
+public class OffersActivity extends AppCompatActivity implements AsyncResponse {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_items);
+
+        Intent intent = getIntent();
+        String type = intent.getStringExtra("type");
+        this.getData(type);
+    }
+
+    public void getData(String type) {
+
+        GetDataAsyncTaskRunner asyncTaskRunner = new GetDataAsyncTaskRunner(this);
+
+        switch (type) {
+            case "meat":
+                asyncTaskRunner.execute(Constants.GET_MEAT_PRODUCTS_URL);
+                break;
+            case "bread":
+                asyncTaskRunner.execute(Constants.GET_BREAD_PRODUCTS_URL);
+                break;
+            case "food":
+                asyncTaskRunner.execute(Constants.GET_FOOD_PRODUCTS_URL);
+                break;
+            case "nonFood":
+                asyncTaskRunner.execute(Constants.GET_NON_FOOD_PRODUCTS_URL);
+                break;
+        }
+
+    }
+
+    public void processData(Object data) {
+
+        String jsonString = (String) data;
+
+        ArrayList<Product> products = JsonParser.parseProductsJson(jsonString);
+
+        ListView listView = (ListView) findViewById(R.id.products_list);
+
+        ProductAdapter productAdapter = new ProductAdapter(this, products);
+
+        listView.setAdapter(productAdapter);
+    }
+}
--- app/src/main/java/com/example/fadi/supermarket/other/Constants.java
+++ app/src/main/java/com/example/fadi/supermarket/other/Constants.java
@@ -7,6 +7,8 @@ public class Constants {
     public static final String GET_FOOD_PRODUCTS_URL = "http://demo2126335.mockable.io/fooditems";
     public static final String GET_NON_FOOD_PRODUCTS_URL = "http://demo2126335.mockable.io/nonfoodproducts";
 
+    public static final String GET_OFFERS_URL = "http://demo2126335.mockable.io/offers";
+
     public static final String SIGN_IN_URL = "http://distributed.ga:8080/api/v1/user/signIn";
     public static final String REGISTER_URL = "http://distributed.ga:8080/api/v1/user/signup";
 }
