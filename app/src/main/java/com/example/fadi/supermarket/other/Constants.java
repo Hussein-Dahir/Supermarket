e5d778d312416eee7e851594f57f5d0fe0218523
Hussein-Dahir <Hussein_Dahir@hotmail.com>
2018-05-17 22:01:58 +0300

    removed un-needed ccostants

--- app/src/main/java/com/example/fadi/supermarket/other/Constants.java
+++ app/src/main/java/com/example/fadi/supermarket/other/Constants.java
@@ -6,11 +6,6 @@ public class Constants {
     public static final String GET_BREAD_PRODUCTS_URL = "http://demo2126335.mockable.io/getbreadproducts";
     public static final String GET_FOOD_PRODUCTS_URL = "http://demo2126335.mockable.io/fooditems";
     public static final String GET_NON_FOOD_PRODUCTS_URL = "http://demo2126335.mockable.io/nonfoodproducts";
-
     public static final String GET_OFFERS_URL = "http://demo2126335.mockable.io/offers";
-
     public static final String GET_COUPONS_URL = "https://demo2126335.mockable.io/coupons";
-
-    public static final String SIGN_IN_URL = "http://distributed.ga:8080/api/v1/user/signIn";
-    public static final String REGISTER_URL = "http://distributed.ga:8080/api/v1/user/signup";
 }
